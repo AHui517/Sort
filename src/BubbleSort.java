495bfeb71acad5a73d9132c2fc7e7d4721a853a7
Asphyxia <1499427353@qq.com>
2020-12-09 13:30:03 +0800

    'InsertSort'

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -16,8 +16,28 @@ public class BubbleSort {
         return array;
     }
 
+    public int[] bubbleSortImprove(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            //flag作为标记，如果前面元素已经有序，则无需继续排序
+            boolean flag=true;
+            //每进行一次排序，下一轮需要排序的的个数就减一
+            for (int j = 1; j < array.length - i; j++) {
+                if (array[j] < array[j - 1]) {
+                    //交换两个元素
+                    flag=false;
+                    int tem = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = tem;
+                }
+            }
+            //前面数组已经有序
+            if (flag) break;
+        }
+        return array;
+    }
+
     public static void main(String[] args) {
         int[] arr={5,4,8,9,6,1,2,3};
-        System.out.println(Arrays.toString(new BubbleSort().bubbleSort(arr)));
+        System.out.println(Arrays.toString(new BubbleSort().bubbleSortImprove(arr)));
     }
 }
---
+++ src/InsertSort.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class InsertSort {
+
+    //该排序和打牌时整自己的手牌过程差不多
+    public int[] insertSort(int[] array) {
+        //从第二个数开始进行插入操作
+        for (int i = 1; i < array.length; i++) {
+            //保存当前需要进行插入操作的元素
+            int t=array[i];
+            //初始化进行计较的索引
+            int j=i-1;
+            //这一步是在寻找我需要插入的位置
+            while(j>=0 && t<array[j]) {
+                array[j+1]=array[j];
+                j--;
+            }
+            //进行插入
+            array[j+1]=t;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] array={45,56,82,1,56,0,63,16,266,4};
+        System.out.println(Arrays.toString(new InsertSort().insertSort(array)));
+    }
+}
